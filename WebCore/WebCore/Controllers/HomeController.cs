da247a36f7ecf42ffaaa5ed3b392a0797b23e4a9
sergei m <sergeim@live.com>
2020-04-11 12:37:42 -0700

    deleting unused cshtml files, and fixing contacts and about pages

--- WebCore/WebCore/Controllers/HomeController.cs
+++ WebCore/WebCore/Controllers/HomeController.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebCore.Models;
 
@@ -18,7 +16,7 @@ namespace WebCore.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Short information about myself:";
 
             return View();
         }
@@ -35,28 +33,6 @@ namespace WebCore.Controllers
             ViewData["Message"] = "Toys action.";
 
             return View();
-        }
-
-        public IActionResult AccessoriesBag()
-        {
-            ViewData["Message"] = "Toys action.";
-
-            return View();
-        }
-
-        public IActionResult AccessoriesGeneric(string folder)
-        {
-            ViewData["Message"] = "Toys action.";
-            var imageFiles = new List<FileData>();
-            var dir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images20191123", "Accessories", "bag");
-            foreach (var file in Directory.GetFiles(dir))
-            {
-                var i = file.IndexOf("images20191123");
-                var relativeFileName = ("/" + file.Substring(i)).Replace("\\", "/");
-
-                imageFiles.Add(new FileData { wwwRootFileName = relativeFileName, });
-            }
-            return View(imageFiles);
         }
 
         public IActionResult ShowSubfolder(string folder, string subfolder)
@@ -80,51 +56,9 @@ namespace WebCore.Controllers
             return View(imageFiles);
         }
 
-        public IActionResult AccessoriesDog()
-        {
-            ViewData["Message"] = "fix me 111.";
-
-            return View();
-        }
-        
-        public IActionResult AccessoriesGlove ()
-        {
-            ViewData["Message"] = "fix me 112.";
-
-            return View();
-        }
-
-        public IActionResult AccessoriesSock()
-        {
-            ViewData["Message"] = "fix me 113.";
-
-            return View();
-        }
-
         public IActionResult Hats()
         {
             ViewData["Message"] = "fix me 114.";
-
-            return View();
-        }
-
-        public IActionResult HatsChild()
-        {
-            ViewData["Message"] = "fix me 115.";
-
-            return View();
-        }
-
-        public IActionResult HatsLady()
-        {
-            ViewData["Message"] = "fix me 116.";
-
-            return View();
-        }
-
-        public IActionResult HatsMen()
-        {
-            ViewData["Message"] = "fix me 117.";
 
             return View();
         }
@@ -136,44 +70,9 @@ namespace WebCore.Controllers
             return View();
         }
 
-        public IActionResult HomeImagesBlanket()
-        {
-            ViewData["Message"] = "HomeImagesBlanket";
-
-            return View();
-        }
-
-        public IActionResult HomeImagesDecor()
-        {
-            ViewData["Message"] = "HomeImagesDecor";
-
-            return View();
-        }
-
-        public IActionResult HomeImagesPillow()
-        {
-            ViewData["Message"] = "HomeImagesPillow";
-
-            return View();
-        }
-
         public IActionResult ShawlsAndScarves()
         {
             ViewData["Message"] = "ShawlsAndScarves";
-
-            return View();
-        }
-
-        public IActionResult Shawl()
-        {
-            ViewData["Message"] = "Shawl";
-
-            return View();
-        }
-
-        public IActionResult Scarf()
-        {
-            ViewData["Message"] = "Scarf";
 
             return View();
         }
@@ -185,44 +84,9 @@ namespace WebCore.Controllers
             return View();
         }
 
-        public IActionResult TopAll()
-        {
-            ViewData["Message"] = "TopAll";
-
-            return View();
-        }
-
         public IActionResult Toys()
         {
             ViewData["Message"] = "Toys action.";
-
-            return View();
-        }
-
-        public IActionResult ToysOctopuses()
-        {
-            ViewData["Message"] = "Toys - Octopuses";
-
-            return View();
-        }
-
-        public IActionResult ToysOpossums()
-        {
-            ViewData["Message"] = "Toys - Opossums";
-
-            return View();
-        }
-
-        public IActionResult ToysTurtles()
-        {
-            ViewData["Message"] = "Toys - Turtles";
-
-            return View();
-        }
-
-        public IActionResult ToysFerret()
-        {
-            ViewData["Message"] = "Toys - Ferrets";
 
             return View();
         }
