02399ebae6db35b9a3a045c3fce4270c93860863
Sergei Meleshchuk <sergeim@microsoft.com>
2021-07-09 15:01:58 -0700

    not working common code

---
+++ WebCore/WebCore/Views/Home/CommonCode.js
@@ -0,0 +1,21 @@
+﻿<script>
+    function RenderImageFolder(div, data) {
+        var len = data.Lines.length;
+        var result = "";
+        for (var i = 0; i < len; i++)
+        {
+                // /images20210703/Fox/DSC04113.JPG
+                var line = data.Lines[i];
+                result += '<table style="display:inline-block">';
+                    result += '<tr><td>';
+                        result += line;
+                        result += '</td></tr>';
+                    result += '<tr><td>';
+                        result += '<img src="' + line + '" style="width:330px;" />';
+                        result += '</td></tr>';
+                    result += '</table>';
+                result += '&nbsp;';
+            }
+        div.innerHTML = result;
+    }
+</script>
