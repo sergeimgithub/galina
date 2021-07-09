3691980e824d2ca4e4798f3aaea9925db1fe656f
Sergei Meleshchuk <sergeim@microsoft.com>
2021-07-09 15:15:08 -0700

    working but the inclusion of the common script CommonCode.js does not work

--- WebCore/WebCore/Views/Home/CommonCode.js
+++ WebCore/WebCore/Views/Home/CommonCode.js
@@ -1,21 +1,20 @@
-﻿<script>
-    function RenderImageFolder(div, data) {
-        var len = data.Lines.length;
-        var result = "";
-        for (var i = 0; i < len; i++)
-        {
-                // /images20210703/Fox/DSC04113.JPG
-                var line = data.Lines[i];
-                result += '<table style="display:inline-block">';
-                    result += '<tr><td>';
-                        result += line;
-                        result += '</td></tr>';
-                    result += '<tr><td>';
-                        result += '<img src="' + line + '" style="width:330px;" />';
-                        result += '</td></tr>';
-                    result += '</table>';
-                result += '&nbsp;';
-            }
-        div.innerHTML = result;
-    }
-</script>
+﻿//. can't include this file into pigs.cshtml file - need to find a way
+function RenderImageFolder(div, data) {
+    var len = data.Lines.length;
+    var result = "";
+    for (var i = 0; i < len; i++)
+    {
+            // /images20210703/Fox/DSC04113.JPG
+            var line = data.Lines[i];
+            result += '<table style="display:inline-block">';
+                result += '<tr><td>';
+                    result += line;
+                    result += '</td></tr>';
+                result += '<tr><td>';
+                    result += '<img src="' + line + '" style="width:330px;" />';
+                    result += '</td></tr>';
+                result += '</table>';
+            result += '&nbsp;';
+        }
+    div.innerHTML = result;
+}
