4687a1ee6fc2698250b5ce9f6f1e5f8acf2b176a
Sergei Meleshchuk <sergeim@microsoft.com>
2021-07-09 17:06:35 -0700

    Moved common javascript code to WebCore/wwwroot/js/CommonCode.js

--- WebCore/WebCore/wwwroot/js/site.js
+++ WebCore/WebCore/wwwroot/js/site.js
@@ -2,3 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//function RenderImageFolder(div, data) {
+//    var len = data.Lines.length;
+//    var result = "";
+//    for (var i = 0; i < len; i++) {
+//        // /images20210703/Fox/DSC04113.JPG
+//        var line = data.Lines[i];
+//        result += '<table style="display:inline-block">';
+//        result += '<tr><td>';
+//        result += line;
+//        result += '</td></tr>';
+//        result += '<tr><td>';
+//        result += '<img src="' + line + '" style="width:330px;" />';
+//        result += '</td></tr>';
+//        result += '</table>';
+//        result += '&nbsp;';
+//    }
+//    div.innerHTML = result;
+//}
