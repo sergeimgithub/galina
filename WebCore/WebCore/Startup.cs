6b2f0731803125bd98710e6b3fbebc1a6df53ad3
Sergei Meleshchuk <sergeim@microsoft.com>
2021-10-22 10:16:23 -0700

    Add the code to accomodate deprecated net core 2.1

--- WebCore/WebCore/Startup.cs
+++ WebCore/WebCore/Startup.cs
@@ -24,11 +24,18 @@ namespace WebCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.Configure<MvcOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.EnableEndpointRouting = false;
             });
 
 
